ed58010637eb69ddabf4a9845c52d67fea27356b
mohamed7431 <mohamedni7431@gmail.com>
2025-08-15 17:43:57 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
- 
-IMAGE_NAME=$1
-IMAGE_TAG=$2
- 
-docker build -t $IMAGE_NAME:$IMAGE_TAG .
-docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
-docker push $IMAGE_NAME:$IMAGE_TAG
